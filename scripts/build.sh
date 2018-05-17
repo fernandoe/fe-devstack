38631bc091aac5a4ae2829f4f3e8e7b0de4fa9e3
Fernando Espíndola <fer.esp@gmail.com>
2018-05-17 17:00:28 -0300

    add projects to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,15 +3,17 @@
 echo "worksparce: $DEVSTACK_WORKSPACE"
 
 projects=(
+    "fe-coaching-server"
     "fe-conta-server"
     "fe-pessoa-server"
     "fe-endereco-server"
     "fe-pagamento-server"
     "fe-sms-server"
+    "fe-task-server"
 )
 
 for p in ${projects[*]}
 do
     echo "  -> Projeto: ${p}"
-    cd $DEVSTACK_WORKSPACE; cd ${p}; make docker-build
+    cd $DEVSTACK_WORKSPACE; cd ${p}; make build
 done
